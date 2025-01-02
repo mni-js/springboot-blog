17f5c07937a8711e369f6669a340c318629d7e8a
mni-js <2ujs4311@gmail.com>
2025-01-02 20:56:07 +0900

    first commit

---
+++ src/main/java/me/mni_js/springboot_blog/Member.java
@@ -0,0 +1,21 @@
+package me.mni_js.springboot_blog;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ src/main/java/me/mni_js/springboot_blog/SpringbootBlogApplication.java
@@ -0,0 +1,13 @@
+package me.mni_js.springboot_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogApplication.class, args);
+	}
+
+}
