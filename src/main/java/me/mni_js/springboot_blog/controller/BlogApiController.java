bbeb701704bc8599ca814b8f1b5803e746a8212f
mni-js <2ujs4311@gmail.com>
2025-01-06 17:04:00 +0900

    dto 작성, get api 구현

--- src/main/java/me/mni_js/springboot_blog/controller/BlogApiController.java
+++ src/main/java/me/mni_js/springboot_blog/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package me.mni_js.springboot_blog.controller;
 import lombok.RequiredArgsConstructor;
 import me.mni_js.springboot_blog.domain.Article;
 import me.mni_js.springboot_blog.dto.AddArticleRequest;
+import me.mni_js.springboot_blog.dto.ArticleResponse;
 import me.mni_js.springboot_blog.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +26,16 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
--- src/main/java/me/mni_js/springboot_blog/service/BlogService.java
+++ src/main/java/me/mni_js/springboot_blog/service/BlogService.java
@@ -6,6 +6,8 @@ import me.mni_js.springboot_blog.dto.AddArticleRequest;
 import me.mni_js.springboot_blog.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
 public class BlogService {
@@ -14,5 +16,9 @@ public class BlogService {
 
     public Article save(AddArticleRequest request) {
         return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
     }
 }
