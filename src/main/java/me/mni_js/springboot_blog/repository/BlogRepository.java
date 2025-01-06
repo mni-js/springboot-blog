4026629d30e9e3b22b977ef578898a2a6b5fd0b2
mni-js <2ujs4311@gmail.com>
2025-01-06 11:50:21 +0900

    복습을 위해 파일 삭제

--- src/main/java/me/mni_js/springboot_blog/repository/BlogRepository.java
+++
@@ -1,7 +0,0 @@
-package me.mni_js.springboot_blog.repository;
-
-import me.mni_js.springboot_blog.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogRepository extends JpaRepository<Article, Long> {
-}
