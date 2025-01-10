2617bea6a671edba1f6b90baade5fc8448214825
mni-js <2ujs4311@gmail.com>
2025-01-10 17:03:18 +0900

    뭐하는건지 모르겠음

---
+++ src/main/java/me/mni_js/springboot_blog/config/WebOAuthSecurityConfig.java
@@ -0,0 +1,108 @@
+package me.mni_js.springboot_blog.config;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+import me.mni_js.springboot_blog.config.jwt.TokenProvider;
+import me.mni_js.springboot_blog.config.oauth.OAuth2AuthorizationRequestBasedOnCookieRepository;
+import me.mni_js.springboot_blog.config.oauth.OAuth2SuccessHandler;
+import me.mni_js.springboot_blog.config.oauth.OAuth2UserCustomService;
+import me.mni_js.springboot_blog.repository.RefreshTokenRepository;
+import me.mni_js.springboot_blog.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebOAuthSecurityConfig {
+    private final OAuth2UserCustomService oAuth2UserCustomService;
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserService userService;
+
+    @Bean
+    public WebSecurityCustomizer configure() { // 스프링 시큐리티 기능 비활성화
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/img/**", "/css/**", "/js/**");
+    }
+
+    @Bean
+    @SuppressWarnings("http")
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // 토큰 방식으로 인증을 하기 때문에 기존에 사용하던 폼로그인, 세션 비활성화
+        http.csrf(csrf -> csrf.disable())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .logout(logout -> logout.disable());
+
+        http.sessionManagement(
+                sessionManagement
+                        -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // 헤더를 확인할 커스텀 필터 추가
+        http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+
+        // 토큰 재발급 URL은 인증 없이 접근 가능하도록 설정. 나머지 API URL은 인증 필요
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/token").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll());
+
+        http.oauth2Login(oauth2 -> oauth2
+                .loginPage("/login")
+                .authorizationEndpoint(authEndpoint -> authEndpoint
+                        .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository())
+                )
+                .successHandler(oAuth2SuccessHandler())
+                .userInfoEndpoint(userInfo -> userInfo
+                        .userService(oAuth2UserCustomService)));
+
+        http.logout(logout -> logout
+                .logoutSuccessUrl("/login"));
+
+        // /api로 시작하는 url인 경우 401 상태 코드를 반환하도록 예외 처리
+        http.exceptionHandling(exceptionHandling -> exceptionHandling
+                .defaultAuthenticationEntryPointFor(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                        new AntPathRequestMatcher("/api/**")));
+
+        return http.build();
+    }
+
+    @Bean
+    public OAuth2SuccessHandler oAuth2SuccessHandler() {
+        return new OAuth2SuccessHandler(tokenProvider,
+                refreshTokenRepository,
+                oAuth2AuthorizationRequestBasedOnCookieRepository(),
+                userService);
+    }
+
+    @Bean
+    public TokenAuthenticationFilter tokenAuthenticationFilter() {
+        return new TokenAuthenticationFilter(tokenProvider);
+    }
+
+    @Bean
+    private OAuth2AuthorizationRequestBasedOnCookieRepository oAuth2AuthorizationRequestBasedOnCookieRepository() {
+        return new OAuth2AuthorizationRequestBasedOnCookieRepository();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
