d617d524e9bd6e4b0c9ecdee0ffab5971e34bb02
mni-js <2ujs4311@gmail.com>
2025-01-10 19:17:45 +0900

    OAuth 뷰 구성

--- src/main/java/me/mni_js/springboot_blog/service/BlogService.java
+++ src/main/java/me/mni_js/springboot_blog/service/BlogService.java
@@ -6,6 +6,7 @@ import me.mni_js.springboot_blog.domain.Article;
 import me.mni_js.springboot_blog.dto.AddArticleRequest;
 import me.mni_js.springboot_blog.dto.UpdateArticleRequest;
 import me.mni_js.springboot_blog.repository.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +31,11 @@ public class BlogService {
     }
 
     public void delete(Long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
@@ -38,8 +43,19 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
     }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
+    }
+
+
 }
